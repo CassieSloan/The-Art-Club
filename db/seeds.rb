fa3510c41ebebc49a338bb32f3e504b88fe24498
Cassie <cassandra.sloan16@gmail.com>
2019-10-30 10:17:53 +1100

    seeded database successfully

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,31 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+for i in 1..30
+    artwork = Content.create(
+        price: rand(500..10000),
+        title: Faker::Music.album,
+    )
+end
+
+for i in 1..30
+    user = User.create(
+        name: Faker::TvShows::DrWho.character,
+        username: Faker::Internet.username,
+        bio: Faker::TvShows::DrWho.quote,
+        email: Faker::Internet.email,
+        password: Faker::Internet.password(min_length: 8)
+    )
+end
+
+for i in 1..30
+    genre = Genre.create(
+        title: Faker::Book.genre
+    )
+end
+
+for i in 1..30
+    medium = Medium.create(
+        material: Faker::Construction.material,
+    )
+end
